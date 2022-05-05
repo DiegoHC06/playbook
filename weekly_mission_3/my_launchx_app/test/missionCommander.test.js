aecf48451d3da25dacf2d6f55214460152b7d3fb
DiegoHC06 <50012419+DiegoHC06@users.noreply.github.com>
2022-05-04 22:40:22 -0500

    Creacion del test

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -1,6 +1,11 @@
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-      const result = 1 + 2
-      expect(result).toBe(10);
+const missionCommander = require('./../app/missionCommander')
+describe("Unit Tests for Mission Commander Class", () => {
+    test('Caso de prueba 1: Creación de objeto', () => {
+
+      //aqui puedes usar el código como lo desea utilizar
+        const woopa = new missionCommander("Woopa")
+
+      // validar el resultado esperando
+        expect(woopa.name).toBe("Woopa");
     });
   })
